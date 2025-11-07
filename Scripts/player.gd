1f52dcf6cd034c5195096c4cf39f892794b62552
Parker <psmi612@wgu.edu>
2025-11-07 16:08:36 -0600

    what i did at work (aiming sens)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,14 +8,25 @@ var equipped_weapon: Node = null
 
 const SMOOTH_SPEED = 10.0
 
-
 var SPEED = 3.0
 const JUMP_VELOCITY = 4.5
 var walking_speed = 3.0
 var running_speed = 5.0
+var aimed_speed = 1.5
 @export var sens_horizontal = 0.0005
 @export var sens_vertical = 0.0005
 
+var aimed := false
+@export var grouping := 0.15
+@export var grouping_aimed = 0.05
+var firing := false
+@export var fire_rate := 0.10
+#get some sort of check for ammmo later on.
+
+func _firing():
+	
+	
+	pass
 
 
 
@@ -25,6 +36,12 @@ func _ready():
 	
 
 func _input(event):
+	if event.is_action_pressed("aim"):
+		aimed = true
+		print(aimed)
+	if event.is_action_released("aim"):
+		aimed = false
+		print(aimed)
 	if event.is_action_pressed("reload"):
 		if equipped_weapon and equipped_weapon.has_method("reload"):
 			equipped_weapon.reload(player_inventory)
@@ -32,11 +49,15 @@ func _input(event):
 		cast_ray_from_camera()
 	if Input.is_action_pressed("esc"):
 		get_tree().quit()
-		
+	
+	var sensmulti := 10
+	if aimed:
+		sensmulti = 50
+	
 	if event is InputEventMouseMotion:
-		rotate_y(rad_to_deg(-event.relative.x*sens_horizontal))
-		visuals.rotate_y(rad_to_deg(event.relative.x*sens_horizontal))
-		camera_mount.rotate_x(rad_to_deg(-event.relative.y*sens_vertical))
+		rotate_y(rad_to_deg(-event.relative.x*sens_horizontal /sensmulti))
+		visuals.rotate_y(rad_to_deg(event.relative.x*sens_horizontal/sensmulti))
+		camera_mount.rotate_x(rad_to_deg(-event.relative.y*sens_vertical /sensmulti))
 
 #INTERACT FUNCTION
 func cast_ray_from_camera():
@@ -64,10 +85,12 @@ func cast_ray_from_camera():
 
 func _physics_process(delta: float) -> void:
 	
-	if Input.is_action_pressed("run") or Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_pressed("run") or Input.is_action_just_pressed("ui_accept") and !aimed:
 		SPEED = running_speed 
 	else:
 		SPEED = walking_speed
+	if aimed:
+		SPEED = aimed_speed
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -80,13 +103,18 @@ func _physics_process(delta: float) -> void:
 	
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var visual_dir = Vector3(input_dir.x,0, input_dir.y).normalized()
+	if aimed:
+		var current_rot = visuals.global_rotation
+		var target_y = camera.global_rotation.y
+		current_rot.y = lerp_angle(current_rot.y, target_y, delta * 8.0)
+		visuals.global_rotation = current_rot
+	
 	if direction.length() > 0.01:
 		input_dir = input_dir.normalized()
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		
-		visuals.rotation.y = lerp_angle(visuals.rotation.y,atan2(-visual_dir.x, -visual_dir.z), delta * SMOOTH_SPEED)
-		#visuals.look_at(position + direction )
+		if !aimed:
+			visuals.rotation.y = lerp_angle(visuals.rotation.y,atan2(-visual_dir.x, -visual_dir.z), delta * SMOOTH_SPEED)
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
