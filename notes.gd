efabe5f2e8f521b18158a314ffce58bae4c26769
Parker Smith <parkersmitty33@gmail.com>
2025-12-08 14:01:06 -0600

    notes added

---
+++ notes.gd
@@ -0,0 +1,13 @@
+extends Node
+# this might be a stupid way to take notes but whatever
+#here are some ideas/things i want to work on 
+#this game will be a PVZ garden warfare meets SWBF 2015/2004/2005
+#class based with each class having some customization on abilites 
+#GAME MODE
+#domination like standard stbw consider making a cart push mode like walker assult or a
+#gardens and grave yards type mode
+
+#CLASSES
+#engeneer, can build robot NPCs that have different types 
+#1 ability type is building 3 different robots can be subed for one strong one ect
+#office class can command 5 npc troops to do what they want
